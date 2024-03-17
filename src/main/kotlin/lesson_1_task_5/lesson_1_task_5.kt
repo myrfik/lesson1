48ca5f586d38e1b699ac3d69b4e508471c227d03
myrfik <lera.ogay.01@bk.ru>
2024-03-17 22:11:38 +0600

    KS-1-5 refactor

--- src/main/kotlin/lesson_1_task_5/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1_task_5/lesson_1_task_5.kt
@@ -3,13 +3,13 @@ package org.example.lesson_1_task_5
 fun main() {
     var secondsOfSpace: Short = 6480
 
-    val hour: Int = secondsOfSpace / secondInHour
-    val remainingSeconds = secondsOfSpace % secondInHour
-    var minutes: Int = remainingSeconds / secondInMinute
-    val seconds: Int = secondsOfSpace % secondInMinute
+    val hour: Int = secondsOfSpace / SECOND_IN_HOUR
+    val remainingSeconds = secondsOfSpace % SECOND_IN_HOUR
+    var minutes: Int = remainingSeconds / SECOND_IN_MINUTE
+    val seconds: Int = secondsOfSpace % SECOND_IN_MINUTE
 
     println(String.format("%02d:%02d:%02d", hour, minutes, seconds))
 }
 
-const val secondInHour = 3600
-const val secondInMinute = 60
+const val SECOND_IN_HOUR = 3600
+const val SECOND_IN_MINUTE = 60
